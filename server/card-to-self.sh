f4edd7d3683a463603ab921c43349f19176c32ff
Graham Boyes <gr.ed.boyes@gmail.com>
2016-04-07 18:59:30 -0400

    Updated params of card-to-self

--- server/card-to-self.sh
+++ server/card-to-self.sh
@@ -1,1 +1,1 @@
-ffmpeg -re -f alsa -ar 44100 -i plughw:CARD=io2,DEV=0 -acodec aac -ab 320k -ac 1 -strict -2 -f flv $1
+ffmpeg -f alsa -ar 44100 -i plughw:CARD=io2,DEV=0 -acodec aac -ab 256k -ac 2 -strict -2 -f flv $1
